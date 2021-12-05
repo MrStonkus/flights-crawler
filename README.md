84f4f9617757913fe1748387e08208bfd1c462a7
MrStonkus <valdas@stonkus.lt>
2021-12-05 15:35:35 +0200

    'init'

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # flights-crawler
+
+Install:
+
+1. Install python >= 3.1v
+2. `pip install requests`
+3.
---
+++ crawler.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = 'https://www.fly540.com/flights/nairobi-to-mombasa?isoneway=0&depairportcode=NBO&arrvairportcode=MBA&date_from=Tue%2C+14+Dec+2021&date_to=Tue%2C+21+Dec+2021&adult_no=1&children_no=0&infant_no=0&currency=USD&searchFlight='
+
+response = requests.get(URL)
+website_html = response.text
+
+soup = BeautifulSoup(website_html, "html.parser")
+
+# flights to one way
+# fligthsFrom = soup.find_all(name="div", class_="fly5-flfrom")
+# resultTo = soup.find_all(name="div", class_="fly5-flto")
+
+# for airport in fligthsFrom:
+#     print(airport.getText().split(' ')[2])
+
+departFlights = soup.find_all(name="div", class_="fly5-flights fly5-depart th")
+print(departFlights.lenght)
